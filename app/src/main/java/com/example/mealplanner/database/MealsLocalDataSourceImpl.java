c1767a071236b55e0b6ccde01e2914ace490f64c
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2024-02-09 11:46:01 +0200

    saving data to realtime database

--- app/src/main/java/com/example/mealplanner/database/MealsLocalDataSourceImpl.java
+++ app/src/main/java/com/example/mealplanner/database/MealsLocalDataSourceImpl.java
@@ -1,31 +1,56 @@
 package com.example.mealplanner.database;
 
 import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.mealplanner.models.Meal;
 import com.example.mealplanner.models.UserManager;
+import com.example.mealplanner.util.NetworkUtils;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MealsLocalDataSourceImpl implements MealsLocalDataSource {
 
     private MealsDAO mealsDAO;
     private Observable<List<Meal>> savedMealsList, plannedMealsList;
     private static MealsLocalDataSourceImpl localDataSource = null;
+    private DatabaseReference databaseReference;
     private UserManager manager;
     String userEmail;
+    String sanitizedEmail;
+    Context context;
 
     private MealsLocalDataSourceImpl(Context context){
         manager = new UserManager();
+        this.context = context;
         mealsDAO = AppDatabase.getInstance(context.getApplicationContext()).getMealDAO();
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("meals");
+
         if(manager.getUser() != null){
             userEmail = manager.getCurrentUserEmail();
+            sanitizedEmail = userEmail.replace(".", "_").replace("#", "_").replace("$", "_").replace("[", "_").replace("]", "_");
+
             savedMealsList = mealsDAO.getAllSavedMeals(userEmail);
             plannedMealsList = mealsDAO.getAllPlannedMeals(userEmail);
         }
     }
+
     public static MealsLocalDataSourceImpl getInstance(Context context){
         if(localDataSource == null){
             localDataSource = new MealsLocalDataSourceImpl(context);
@@ -41,24 +66,41 @@ public class MealsLocalDataSourceImpl implements MealsLocalDataSource {
     public void insertSavedMeal(Meal meal) {
         meal.setDbType("Saved");
         meal.setUserEmail(userEmail);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                mealsDAO.insertSavedMeal(meal);
-            }
-        }).start();
+        Observable.fromCallable(() -> {
+                    mealsDAO.insertSavedMeal(meal);
+                    return true;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        result -> {
+                            databaseReference.child(sanitizedEmail).child("savedMeals").child(meal.getIdMeal()).setValue(meal);
+                        },
+                        error -> {
+                            // Handle error
+                            Log.e("TAG", "Error inserting saved meal: " + error.getMessage());
+                        }
+                );
     }
 
     @Override
     public void deleteSavedMeal(Meal meal) {
         meal.setDbType("Saved");
         meal.setUserEmail(userEmail);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                mealsDAO.deleteSavedMeal(meal);
-            }
-        }).start();
+        Observable.fromCallable(() -> {
+                    mealsDAO.deleteSavedMeal(meal);
+                    return true;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        result -> {
+                            databaseReference.child(sanitizedEmail).child("savedMeals").child(meal.getIdMeal()).removeValue();
+                        },
+                        error -> {
+                            Log.e("TAG", "Error inserting saved meal: " + error.getMessage());
+                        }
+                );
     }
 
 
@@ -86,25 +128,43 @@ public class MealsLocalDataSourceImpl implements MealsLocalDataSource {
     public void insertPlannedMeal(Meal meal) {
         meal.setDbType("Planned");
         meal.setUserEmail(userEmail);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                mealsDAO.insertPlannedMeal(meal);
-            }
-        }).start();
+        Observable.fromCallable(() -> {
+                    mealsDAO.insertSavedMeal(meal);
+                    return true;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        result -> {
+                            databaseReference.child(sanitizedEmail).child("plannedMeals").child(meal.getPlanDate()).setValue(meal);
+                        },
+                        error -> {
+                            Log.e("TAG", "Error inserting saved meal: " + error.getMessage());
+                        }
+                );
     }
 
     @Override
     public void deletePlannedMeal(Meal meal) {
         meal.setDbType("Planned");
         meal.setUserEmail(userEmail);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                mealsDAO.deletePlannedMeal(meal);
-            }
-        }).start();
+        Observable.fromCallable(() -> {
+                    mealsDAO.deletePlannedMeal(meal);
+                    return true;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        result -> {
+                            databaseReference.child(sanitizedEmail).child("plannedMeals").child(meal.getPlanDate()).removeValue();
+                        },
+                        error -> {
+                            Log.e("TAG", "Error inserting saved meal: " + error.getMessage());
+                        }
+                );
     }
+
+
 
 
 }
